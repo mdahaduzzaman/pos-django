1d0782f43abf1f95503b63a3195bf71d91df0382
Md Ahaduzzaman <mdahaduzzamanhridoy@gmail.com>
2025-04-24 21:45:24 +0600

    auto groups creation implement

--- app/signals.py
+++ app/signals.py
@@ -1,5 +1,6 @@
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_migrate
+from django.contrib.auth.models import Group
 
 from app.models import Product
 
@@ -8,3 +9,11 @@ from app.models import Product
 def calculate_load_mileage_and_save(sender, instance: Product, created: bool, **kwargs):
     if instance.current_stock < instance.reorder_threshold:
         print("Reorder Needed")
+
+
+@receiver(post_migrate)
+def schedule_audit_logs(sender, **kwargs):
+    if sender.name == "app":
+        groups = ["manager"]
+        for group in groups:
+            Group.objects.get_or_create(name=group)
